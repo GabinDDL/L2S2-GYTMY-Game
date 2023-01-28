07f74b3d2cd258a1dd2b6401b18ce3a3c9133c75
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-01-28 21:37:01 +0100

    TODO: Re-implement

--- src/main/java/com/gytmy/labyrinth/LabyrinthModel1D.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthModel1D.java
@@ -8,9 +8,10 @@ import java.util.Arrays;
     An array of dimension 1 containing booleans is used to reprensent it
     because it can be considered the same as a segmentend segment
  */
-public class LabyrinthModel1D implements InterfaceLabyrinthModel {
+public class LabyrinthModel1D implements LabyrinthModel {
 
     private boolean[] board; // 1D Board is represented by a segment
+    private Player[] players; // list of players
 
     /**
      * @param length of the 1D Labyrinth
@@ -41,28 +42,42 @@ public class LabyrinthModel1D implements InterfaceLabyrinthModel {
         return board;
     }
 
+    // TODO: Adapt the functions by using the walls" booleans instead of using
+    // coordinates
     @Override
     public boolean isMoveValid(Player player, Direction direction) {
-        // TODO Auto-generated method stub
-        return false;
+        switch (direction) {
+            case LEFT:
+                return player.getCoordinates()[0] > 0;
+            case RIGHT:
+                return player.getCoordinates()[0] < board.length - 1;
+            default:
+                return false;
+        }
     }
 
     @Override
     public void movePlayer(Player player, Direction direction) {
-        // TODO Auto-generated method stub
+        if (!isMoveValid(player, direction))
+            return;
 
+        player.move(direction);
     }
 
     @Override
     public boolean isPlayerAtExit(Player player) {
-        // TODO Auto-generated method stub
-        return false;
+        int position = player.getCoordinates()[0];
+        return position == getBoard().length - 1;
     }
 
     @Override
     public boolean isGameOver() {
-        // TODO Auto-generated method stub
-        return false;
+        for (Player player : players) {
+            if (!isPlayerAtExit(player))
+                return false;
+        }
+
+        return true;
     }
 
 }
