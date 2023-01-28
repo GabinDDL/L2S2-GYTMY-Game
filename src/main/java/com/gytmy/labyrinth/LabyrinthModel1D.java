2d1420dfdf86a4d1efdfe8e34000e85b6d4b1b2a
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-01-28 16:21:32 +0100

    Implemented 1D Labyrinth Model

--- src/main/java/com/gytmy/labyrinth/LabyrinthModel1D.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthModel1D.java
@@ -1,11 +1,39 @@
 package com.gytmy.labyrinth;
 
+import java.util.Arrays;
+
 public class LabyrinthModel1D implements InterfaceLabyrinthModel {
+
+    private boolean board[]; // 1D Board is represented by a segment
+    private int start; // The index of the entrance
+    private int end; // The index of the exit
+
+    public LabyrinthModel1D(boolean[] board, int start, int end) {
+        this.board = board;
+        this.start = start;
+        this.end = end;
+    }
+
+    public LabyrinthModel1D(int length) {
+        initBoard(length);
+        start = 0;
+        end = length - 1;
+    }
+
+    /**
+     * Initializes the 1-Dimensional board with the given length
+     * by filling with `true` as it is a line without walls
+     * 
+     * @param length of the labyrinth
+     */
+    public void initBoard(int length) {
+        board = new boolean[length];
+        Arrays.fill(board, true);
+    }
 
     @Override
     public boolean[] getBoard() {
-        // TODO Auto-generated method stub
-        return null;
+        return board;
     }
 
     @Override
