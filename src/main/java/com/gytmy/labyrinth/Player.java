e25224fa9daa4c0d4cf0b1427d9142b8e9ef4fe9
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-01-30 00:43:38 +0100

    Implemented Player

--- src/main/java/com/gytmy/labyrinth/Player.java
+++ src/main/java/com/gytmy/labyrinth/Player.java
@@ -2,7 +2,17 @@ package com.gytmy.labyrinth;
 
 public interface Player {
 
+    public int getX();
+
+    public int getY();
+
     public int[] getCoordinates();
+
+    public void setX(int x);
+
+    public void setY(int y);
+
+    public void setCoordinates(int[] coordinates);
 
     public void move(Direction direction);
 }
---
+++ src/main/java/com/gytmy/labyrinth/PlayerImplementation.java
@@ -0,0 +1,74 @@
+package com.gytmy.labyrinth;
+
+public class PlayerImplementation implements Player {
+    private int x, y;
+
+    public PlayerImplementation(int[] coordinates) {
+        this(coordinates[0], coordinates[1]);
+    }
+
+    public PlayerImplementation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Constructor for the player in 1 Dimension
+     * 
+     * @param x
+     */
+    public PlayerImplementation(int x) {
+        this(x, 1);
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int[] getCoordinates() {
+        return new int[] { x, y };
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public void setCoordinates(int[] coordinates) {
+        this.x = coordinates[0];
+        this.y = coordinates[1];
+    }
+
+    @Override
+    public void move(Direction direction)
+            throws IllegalArgumentException {
+        switch (direction) {
+            case LEFT:
+            case RIGHT:
+                int newHorizontalPosition = x + direction.getStep();
+                setX(newHorizontalPosition);
+                break;
+            case UP:
+            case DOWN:
+                int newVerticalPosition = y + direction.getStep();
+                setY(newVerticalPosition);
+                break;
+            default:
+                throw new IllegalArgumentException("Direction " + direction + " is not supported");
+        }
+    }
+
+}
