f5680e5fc190c9997893ef5b231bd6e4c0c20592
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-06 22:30:53 +0100

    Add new methods to ArrayOperations

--- src/main/java/com/gytmy/utils/ArrayOperations.java
+++ src/main/java/com/gytmy/utils/ArrayOperations.java
@@ -34,4 +34,50 @@ public class ArrayOperations {
         }
     }
 
+    public static boolean isColumnEmpty(boolean[][] array, int i) {
+        for (int row = 0; row < array.length; ++row) {
+            if (array[row][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isRowEmpty(boolean[][] array, int i) {
+        for (int col = 0; col < array[i].length; ++col) {
+            if (array[i][col]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean[][] addEmptyColumn(boolean[][] array, int index) {
+        boolean[][] newArray = new boolean[array.length][array[0].length + 1];
+        for (int row = 0; row < array.length; ++row) {
+            for (int col = 0; col < array[row].length; ++col) {
+                if (col < index) {
+                    newArray[row][col] = array[row][col];
+                } else {
+                    newArray[row][col + 1] = array[row][col];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    public static boolean[][] addEmptyRow(boolean[][] array, int index) {
+        boolean[][] newArray = new boolean[array.length + 1][array[0].length];
+        for (int row = 0; row < array.length; ++row) {
+            for (int col = 0; col < array[row].length; ++col) {
+                if (row < index) {
+                    newArray[row][col] = array[row][col];
+                } else {
+                    newArray[row + 1][col] = array[row][col];
+                }
+            }
+        }
+        return newArray;
+    }
+
 }
