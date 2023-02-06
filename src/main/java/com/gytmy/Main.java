8c3f53075d629c4f30757b7e432bf73a7f77c8bd
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-06 23:34:15 +0100

    remove old Main test

--- src/main/java/com/gytmy/Main.java
+++ src/main/java/com/gytmy/Main.java
@@ -1,16 +1,9 @@
 package com.gytmy;
 
-import com.gytmy.labyrinth.generators.DepthFirstGenerator;
-import com.gytmy.utils.Boolean2DArraysOperations;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
-        boolean[][] board = new DepthFirstGenerator(41, 21).generate();
-
-        Boolean2DArraysOperations.printBoolean2DArray(board);
     }
 
 }
