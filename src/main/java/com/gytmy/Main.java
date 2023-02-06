23bf84c7cfc5a6dcc4a0ade3b9e78f0ee8871780
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-06 20:47:19 +0100

    refactor algorithm

--- src/main/java/com/gytmy/Main.java
+++ src/main/java/com/gytmy/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        boolean[][] board = new DepthFirstGenerator().generate(41, 21);
+        boolean[][] board = new DepthFirstGenerator().generate(41, 20);
         ArrayOperations.printBoolean2DArray(board);
     }
 
