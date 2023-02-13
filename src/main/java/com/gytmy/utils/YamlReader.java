04014ec69dd186457edbdeee21921513f7119b13
Yanis <yanis.lacenne@gmail.com>
2023-02-13 21:28:16 +0100

    YamlReader Read/Write default

--- src/main/java/com/gytmy/utils/YamlReader.java
+++ src/main/java/com/gytmy/utils/YamlReader.java
@@ -1,17 +1,13 @@
 package com.gytmy.utils;
 
 import java.io.File;
-import java.util.ArrayList;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import com.gytmy.sound.ApplicationConfig;
 import com.gytmy.sound.User;
 
 public class YamlReader {
-
-    private static String dataStorageFilePath = "src/main/java/com/gytmy/config.yaml";
 
     /****************************** READ ***************************/
     // public static void main(String[] args) throws Exception {
@@ -40,6 +36,62 @@ public class YamlReader {
     // }
     /****************************** WRITE ***************************/
 
+    /**
+     * Read the .yaml config file
+     * 
+     * @return ApplicationConfig associatesd
+     * @throws Exception
+     */
+    public static User read(String filePath) throws Exception {
+        handleInvalidFilePath(filePath);
+
+        File file = new File(filePath);
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+        User user = objectMapper.readValue(file,
+                User.class);
+        return user;
+    }
+
+    /**
+     * Write the .yaml config file the user
+     */
+    public static void write(String filePath, User user) throws Exception {
+        handleInvalidFilePath(filePath);
+
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        objectMapper.writeValue(new File(filePath), user);
+    }
+
+    /**
+     * Handle invalid exceptions
+     */
+    private static void handleInvalidFilePath(String filePath) {
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("Invalid file path");
+        }
+
+        if (!filePath.endsWith(".yaml")) {
+            throw new IllegalArgumentException("Invalid file path");
+        }
+
+        if (fileDoesNotExists(filePath)) {
+            throw new IllegalArgumentException("File does not exists");
+        }
+    }
+
+    /**
+     * Check if the file exists
+     */
+    private static boolean fileDoesNotExists(String filePath) {
+        return !(new File(filePath)).exists();
+    }
+
     public static void main(String[] args) throws Exception {
+
+        String pathExample = "src/resources/audioFiles/Yago/config.yaml";
+
+        User user = read(pathExample);
+        System.out.println("User's Info :" + user.toString());
     }
 }
