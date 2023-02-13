b5d906837620e2033e613c4a34859ae8e9cd0638
Yanis <yanis.lacenne@gmail.com>
2023-02-14 00:22:21 +0100

    Added blank verification on filePath during writing

--- src/main/java/com/gytmy/utils/YamlReader.java
+++ src/main/java/com/gytmy/utils/YamlReader.java
@@ -8,33 +8,6 @@ import com.gytmy.sound.User;
 
 public class YamlReader {
 
-    /****************************** READ ***************************/
-    // public static void main(String[] args) throws Exception {
-    // File file = new File(dataStorageFilePath);
-    // ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-
-    // ApplicationConfig config = objectMapper.readValue(file,
-    // ApplicationConfig.class);
-    // System.out.println("Application config info " + config.toString());
-    // }
-    /****************************** READ ***************************/
-
-    /****************************** WRITE ***************************/
-    // public static void main(String[] args) throws Exception {
-
-    // ArrayList<User> users = new ArrayList<User>();
-
-    // User newUser = new User("Yago");
-    // users.add(newUser);
-
-    // ApplicationConfig appConfig = new ApplicationConfig();
-
-    // ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-    // objectMapper.writeValue(new
-    // File(dataStorageFilePath), appConfig);
-    // }
-    /****************************** WRITE ***************************/
-
     /**
      * Read the .yaml config file
      * 
@@ -66,7 +39,7 @@ public class YamlReader {
      * Handle invalid exceptions
      */
     private static void handleInvalidFilePath(String filePath, boolean shouldExists) {
-        if (filePath == null || filePath.isEmpty()) {
+        if (filePath == null || filePath.isEmpty() || filePath.isBlank()) {
             throw new IllegalArgumentException("Invalid file path");
         }
 
