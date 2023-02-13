384e1eb0af70a1d6ba05bbb12a40c657cdd8c570
Yanis <yanis.lacenne@gmail.com>
2023-02-13 22:03:15 +0100

    Add User Invalid Arguments to constructor

--- src/main/java/com/gytmy/sound/User.java
+++ src/main/java/com/gytmy/sound/User.java
@@ -7,6 +7,8 @@ public class User {
     protected int numEtu;
 
     public User(String firstname, String lastname, int numEtu) {
+        handleInvalidArguments(firstname, lastname, numEtu);
+
         this.firstname = firstname;
         this.lastname = lastname;
         this.numEtu = numEtu;
@@ -18,6 +20,40 @@ public class User {
 
     public User(String firstname) {
         this(firstname, "Default");
+    }
+
+    public User() {
+        this("NoName");
+    }
+
+    private void handleInvalidArguments(String firstname, String lastname, int numEtu) {
+        if (nameIsInvalid(firstname)) {
+            throw new IllegalArgumentException("Invalid firstname");
+        }
+
+        if (nameIsInvalid(lastname)) {
+            throw new IllegalArgumentException("Invalid lastname");
+        }
+
+        if (numEtu < 0) {
+            throw new IllegalArgumentException("Invalid numEtu");
+        }
+    }
+
+    private boolean nameIsInvalid(String name) {
+        if (name == null || name.isEmpty()) {
+            return true;
+        }
+        return isEntirelySpace(name);
+    }
+
+    private boolean isEntirelySpace(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            if (!Character.isSpaceChar(name.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getFirstname() {
