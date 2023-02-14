beb77bb3e89f6f04a08c65304a53c2059d9b44f1
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-02-14 10:16:28 +0100

    Fixed LabyrinthTilePanel

--- src/main/java/com/gytmy/labyrinth/LabyrinthTilePanel.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthTilePanel.java
@@ -1,32 +1,34 @@
 package com.gytmy.labyrinth;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
 public class LabyrinthTilePanel extends JPanel {
 
-  private LabyrinthModel model;
   private boolean[][] board;
+  private int nbRowsBoard;
+  private int nbColsBoard;
+  private static final int CELL_SIZE = 20;
 
   public LabyrinthTilePanel(LabyrinthModel model) {
-    this.model = model;
+    board = model.getBoard();
+    nbRowsBoard = board.length;
+    nbColsBoard = board[0].length;
+
     updateTilePanel();
+    setPreferredSize(new Dimension(nbColsBoard * CELL_SIZE, nbRowsBoard * CELL_SIZE));
   }
 
   public void updateTilePanel() {
-    boolean[][] board = model.getBoard();
-    int nbRowsBoard = board.length;
-    int nbColsBoard = board[0].length;
-
     GridLayout grid = new GridLayout(nbRowsBoard, nbColsBoard);
     setLayout(grid);
-    tileLabyrinth(nbRowsBoard, nbColsBoard);
-
+    tileLabyrinth();
   }
 
-  private void tileLabyrinth(int nbRowsBoard, int nbColsBoard) {
+  private void tileLabyrinth() {
     for (int row = 0; row < nbRowsBoard; row++) {
       for (int col = 0; col < nbColsBoard; col++) {
         tileFloor(row, col);
@@ -37,13 +39,12 @@ public class LabyrinthTilePanel extends JPanel {
   private void tileFloor(int row, int col) {
     GridLayout cellGrid = new GridLayout(2, 2);
     JPanel cell = new JPanel(cellGrid);
-
+    cell.setSize(CELL_SIZE, CELL_SIZE);
     if (board[row][col]) {
       cell.setBackground(Color.WHITE);
     } else {
       cell.setBackground(Color.BLACK);
     }
-
     add(cell);
   }
 
