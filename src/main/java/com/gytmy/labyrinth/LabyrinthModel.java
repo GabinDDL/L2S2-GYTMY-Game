2fa1192f8133d25f2fc08abb382b91da2dcdd764
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-02-14 10:14:43 +0100

    Added getters for initialCell and exitCell

--- src/main/java/com/gytmy/labyrinth/LabyrinthModel.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthModel.java
@@ -1,51 +1,57 @@
 package com.gytmy.labyrinth;
 
+import com.gytmy.utils.Vector2;
+
 public interface LabyrinthModel {
 
-    /**
-     * Gets the copy of the board representing the Labyrinth,
-     * 
-     * @return the Board according to its Dimension
-     */
-    boolean[][] getBoard();
+  /**
+   * Gets the copy of the board representing the Labyrinth,
+   * 
+   * @return the Board according to its Dimension
+   */
+  boolean[][] getBoard();
 
-    /**
-     * Checks if the given move is valid for the given player
-     * depending on its position on the board
-     * 
-     * The criteria:
-     * A move is valid when the future position of the player,
-     * after the move, won't be outside of the board
-     * and won't be a wall
-     * 
-     * @param player
-     * @param direction of the move
-     * @return true if the move is valid according to the criteria;
-     *         false otherwise
-     */
-    boolean isMoveValid(Player player, Direction direction);
+  Vector2 getInitialCell();
 
-    /**
-     * Moves the given player to the given direction
-     * 
-     * @param player
-     * @param direction
-     */
-    void movePlayer(Player player, Direction direction);
+  Vector2 getExitCell();
 
-    /**
-     * Checks if the given player's position is on the exit's position
-     * 
-     * @param player
-     * @return true if the player is at the exit;
-     *         false otherwise
-     */
-    boolean isPlayerAtExit(Player player);
+  /**
+   * Checks if the given move is valid for the given player
+   * depending on its position on the board
+   * 
+   * The criteria:
+   * A move is valid when the future position of the player,
+   * after the move, won't be outside of the board
+   * and won't be a wall
+   * 
+   * @param player
+   * @param direction of the move
+   * @return true if the move is valid according to the criteria;
+   *         false otherwise
+   */
+  boolean isMoveValid(Player player, Direction direction);
 
-    /**
-     * @return true if the game has ended;
-     *         false otherwise
-     */
-    boolean isGameOver();
+  /**
+   * Moves the given player to the given direction
+   * 
+   * @param player
+   * @param direction
+   */
+  void movePlayer(Player player, Direction direction);
+
+  /**
+   * Checks if the given player's position is on the exit's position
+   * 
+   * @param player
+   * @return true if the player is at the exit;
+   *         false otherwise
+   */
+  boolean isPlayerAtExit(Player player);
+
+  /**
+   * @return true if the game has ended;
+   *         false otherwise
+   */
+  boolean isGameOver();
 
 }
--- src/main/java/com/gytmy/labyrinth/LabyrinthModelImplementation.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthModelImplementation.java
@@ -24,267 +24,273 @@ import com.gytmy.utils.Vector2;
  */
 public class LabyrinthModelImplementation implements LabyrinthModel {
 
-    protected boolean[][] board;
-    protected Vector2 initialCell;
-    protected Vector2 exitCell;
+  protected boolean[][] board;
+  protected Vector2 initialCell;
+  protected Vector2 exitCell;
 
-    protected Player[] players;
+  protected Player[] players;
 
-    public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Vector2 initialCell,
-            Vector2 exitCell,
-            Player[] players) {
-        this(generator.generate(width, height, initialCell), initialCell, exitCell, players);
+  public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Vector2 initialCell,
+      Vector2 exitCell,
+      Player[] players) {
+    this(generator.generate(width, height, initialCell), initialCell, exitCell, players);
+  }
+
+  public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Vector2 initialCell,
+      Player[] players) {
+    this(generator.generate(width, height, initialCell), initialCell, null, players);
+  }
+
+  public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Player[] players) {
+    this(generator.generate(width, height), null, null, players);
+  }
+
+  public LabyrinthModelImplementation(int width, int height, Player[] players) {
+    this(new DepthFirstGenerator(), width, height, players);
+  }
+
+  public LabyrinthModelImplementation(boolean[][] board, Vector2 initialCell, Vector2 exitCell, Player[] players) {
+    handleNullArguments(board);
+    handleInvalidBoardSize(board);
+    this.board = Boolean2DArraysOperations.copy(board);
+    this.initialCell = determineInitialCell(initialCell);
+    this.exitCell = determineExitCell(exitCell);
+    this.players = players;
+  }
+
+  /**
+   * Checks if the arguments are null. If they are, it throws an
+   * IllegalArgumentException.
+   * 
+   * @param board
+   */
+  private void handleNullArguments(boolean[][] board) {
+    if (board == null) {
+      throw new IllegalArgumentException("Board cannot be null");
+    }
+  }
+
+  /**
+   * Checks if the board is too small. If it is, it throws an
+   * IllegalArgumentException.
+   * 
+   * @param board
+   */
+  private void handleInvalidBoardSize(boolean[][] board) {
+    if (board.length < 3) {
+      throw new IllegalArgumentException("Board must have at least 3 rows");
+    }
+    if (board[0].length < 3) {
+      throw new IllegalArgumentException("Board must have at least 3 columns");
+    }
+  }
+
+  /**
+   * Determines the initial cell. If the initial cell is null, it gets a
+   * random non-wall cell. If the initial cell is outside the board or is a
+   * wall, it throws an IllegalArgumentException.
+   * 
+   * @param initialCell
+   * @return the initial cell
+   */
+  private Vector2 determineInitialCell(Vector2 initialCell) {
+    if (initialCell == null) {
+      // Get a random non-wall cell
+      LabyrinthCellFinder finder = new LabyrinthCellFinder(board);
+      initialCell = finder.getClosestToTopCell();
+    }
+    handleInvalidStartCell(initialCell);
+    return initialCell;
+  }
+
+  /**
+   * Checks if the initial and exit cells are valid. If they are not, it throws
+   * an IllegalArgumentException.
+   * 
+   * @param initialCell
+   */
+  private void handleInvalidStartCell(Vector2 initialCell) {
+    if (isOutsideBounds(initialCell)) {
+      throw new IllegalArgumentException("Initial cell is outside the board");
     }
 
-    public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Vector2 initialCell,
-            Player[] players) {
-        this(generator.generate(width, height, initialCell), initialCell, null, players);
+    if (!board[initialCell.getY()][initialCell.getX()]) {
+      throw new IllegalArgumentException("Initial cell is a wall");
     }
 
-    public LabyrinthModelImplementation(BoardGenerator generator, int width, int height, Player[] players) {
-        this(generator.generate(width, height), null, null, players);
+    if (initialCell.equals(exitCell)) {
+      throw new IllegalArgumentException("Initial and exit cells cannot be the same");
+    }
+  }
+
+  /**
+   * Determines the exit cell. If it is null, it will be set to the furthest
+   * cell from the initial cell.
+   * 
+   * @param exitCell\
+   * @return the exit cell
+   */
+  private Vector2 determineExitCell(Vector2 exitCell) {
+    if (exitCell == null) {
+      LabyrinthCellFinder finder = new LabyrinthCellFinder(board);
+      exitCell = finder.getFurthestCell(initialCell);
+    }
+    handleInvalidExitCell(exitCell);
+    return exitCell;
+  }
+
+  /**
+   * Checks if the exit cell is valid. If it is not, it throws an
+   * IllegalArgumentException.
+   * 
+   * @param exitCell
+   */
+  private void handleInvalidExitCell(Vector2 exitCell) {
+    if (isOutsideBounds(exitCell)) {
+      throw new IllegalArgumentException("Exit cell is outside the board");
     }
 
-    public LabyrinthModelImplementation(int width, int height, Player[] players) {
-        this(new DepthFirstGenerator(), width, height, players);
+    if (!board[exitCell.getY()][exitCell.getX()]) {
+      throw new IllegalArgumentException("Exit cell is a wall");
+    }
+    if (exitCell.equals(initialCell)) {
+      throw new IllegalArgumentException("Initial and exit cells cannot be the same");
+    }
+  }
+
+  private boolean isOutsideBounds(Vector2 cell) {
+    return cell.getX() < 0 || cell.getX() >= board[0].length ||
+        cell.getY() < 0 || cell.getY() >= board.length;
+  }
+
+  @Override
+  public boolean[][] getBoard() {
+    if (board == null) {
+      return new boolean[0][0];
+    }
+    return Boolean2DArraysOperations.copy(board);
+  }
+
+  @Override
+  public Vector2 getInitialCell() {
+    return initialCell;
+  }
+
+  @Override
+  public Vector2 getExitCell() {
+    return exitCell;
+  }
+
+  @Override
+  public boolean isMoveValid(Player player, Direction direction) {
+    return !isGoingOutside(player, direction) &&
+        !isGoingIntoWall(player, direction);
+  }
+
+  /**
+   * Checks if the given player will end up outside of the labyrinth
+   * if he makes the move with the given direction
+   * 
+   * Here the board is represented as a horizontal segment
+   * so it is possible to move horizontally
+   * but moving vertically isn't because there are borders which are walls
+   * 
+   * @param player
+   * @param direction
+   * @return true if the move will move the player outside;
+   *         otherwise false
+   */
+  private boolean isGoingOutside(Player player, Direction direction) {
+    switch (direction) {
+      case UP:
+        return player.getY() <= 0;
+      case DOWN:
+        return player.getY() >= board.length - 1;
+      case LEFT:
+        return player.getX() <= 0;
+      case RIGHT:
+        return player.getX() >= board[1].length - 1;
+      default:
+        return false;
+    }
+  }
+
+  /**
+   * Checks if the given player will end up in a wall
+   * if he makes the move with the given direction
+   * 
+   * Here the board is represented as a horizontal segment
+   * so it is possible to move horizontally
+   * but moving vertically isn't because there are borders which are walls
+   * 
+   * @param player
+   * @param direction
+   * @return true if newPosition is a wall;
+   *         false otherwise
+   */
+  private boolean isGoingIntoWall(Player player, Direction direction)
+      throws IllegalArgumentException {
+    int newX = player.getX();
+    int newY = player.getY();
+    switch (direction) {
+      case UP:
+      case DOWN:
+        newY = player.getY() + direction.getStep();
+        break;
+      case LEFT:
+      case RIGHT:
+        newX = player.getX() + direction.getStep();
+        break;
+      default:
+        throw new IllegalArgumentException("Direction " + direction + " is not supported");
+    }
+    return isWall(newX, newY);
+  }
+
+  /**
+   * @param x
+   * @param y
+   * @return true if there is a wall at the given coordinates;
+   *         false otherwise
+   */
+  private boolean isWall(int x, int y) {
+    return !board[y][x];
+  }
+
+  @Override
+  public void movePlayer(Player player, Direction direction) {
+    if (isMoveValid(player, direction)) {
+      player.move(direction);
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.gytmy.labyrinth.LabyrinthModel#isGameOver()
+   * 
+   * Here, the game is considered over when
+   * all the players have made it to the exit
+   * 
+   * May be modified to fit the definition of a game
+   */
+  @Override
+  public boolean isGameOver() {
+    if (players == null || players.length == 0) {
+      return true;
     }
 
-    public LabyrinthModelImplementation(boolean[][] board, Vector2 initialCell, Vector2 exitCell, Player[] players) {
-        handleNullArguments(board);
-        handleInvalidBoardSize(board);
-        this.board = Boolean2DArraysOperations.copy(board);
-        this.initialCell = determineInitialCell(initialCell);
-        this.exitCell = determineExitCell(exitCell);
-        this.players = players;
+    for (Player player : players) {
+      if (!isPlayerAtExit(player))
+        return false;
     }
 
-    /**
-     * Checks if the arguments are null. If they are, it throws an
-     * IllegalArgumentException.
-     * 
-     * @param board
-     */
-    private void handleNullArguments(boolean[][] board) {
-        if (board == null) {
-            throw new IllegalArgumentException("Board cannot be null");
-        }
-    }
+    return true;
+  }
 
-    /**
-     * Checks if the board is too small. If it is, it throws an
-     * IllegalArgumentException.
-     * 
-     * @param board
-     */
-    private void handleInvalidBoardSize(boolean[][] board) {
-        if (board.length < 3) {
-            throw new IllegalArgumentException("Board must have at least 3 rows");
-        }
-        if (board[0].length < 3) {
-            throw new IllegalArgumentException("Board must have at least 3 columns");
-        }
-    }
-
-    /**
-     * Determines the initial cell. If the initial cell is null, it gets a
-     * random non-wall cell. If the initial cell is outside the board or is a
-     * wall, it throws an IllegalArgumentException.
-     * 
-     * @param initialCell
-     * @return the initial cell
-     */
-    private Vector2 determineInitialCell(Vector2 initialCell) {
-        if (initialCell == null) {
-            // Get a random non-wall cell
-            LabyrinthCellFinder finder = new LabyrinthCellFinder(board);
-            initialCell = finder.getClosestToTopCell();
-        }
-        handleInvalidStartCell(initialCell);
-        return initialCell;
-    }
-
-    /**
-     * Checks if the initial and exit cells are valid. If they are not, it throws
-     * an IllegalArgumentException.
-     * 
-     * @param initialCell
-     */
-    private void handleInvalidStartCell(Vector2 initialCell) {
-        if (isOutsideBounds(initialCell)) {
-            throw new IllegalArgumentException("Initial cell is outside the board");
-        }
-
-        if (!board[initialCell.getY()][initialCell.getX()]) {
-            throw new IllegalArgumentException("Initial cell is a wall");
-        }
-
-        if (initialCell.equals(exitCell)) {
-            throw new IllegalArgumentException("Initial and exit cells cannot be the same");
-        }
-    }
-
-    /**
-     * Determines the exit cell. If it is null, it will be set to the furthest
-     * cell from the initial cell.
-     * 
-     * @param exitCell\
-     * @return the exit cell
-     */
-    private Vector2 determineExitCell(Vector2 exitCell) {
-        if (exitCell == null) {
-            LabyrinthCellFinder finder = new LabyrinthCellFinder(board);
-            exitCell = finder.getFurthestCell(initialCell);
-        }
-        handleInvalidExitCell(exitCell);
-        return exitCell;
-    }
-
-    /**
-     * Checks if the exit cell is valid. If it is not, it throws an
-     * IllegalArgumentException.
-     * 
-     * @param exitCell
-     */
-    private void handleInvalidExitCell(Vector2 exitCell) {
-        if (isOutsideBounds(exitCell)) {
-            throw new IllegalArgumentException("Exit cell is outside the board");
-        }
-
-        if (!board[exitCell.getY()][exitCell.getX()]) {
-            throw new IllegalArgumentException("Exit cell is a wall");
-        }
-        if (exitCell.equals(initialCell)) {
-            throw new IllegalArgumentException("Initial and exit cells cannot be the same");
-        }
-    }
-
-    private boolean isOutsideBounds(Vector2 cell) {
-        return cell.getX() < 0 || cell.getX() >= board[0].length ||
-                cell.getY() < 0 || cell.getY() >= board.length;
-    }
-
-    @Override
-    public boolean[][] getBoard() {
-        if (board == null) {
-            return new boolean[0][0];
-        }
-        return Boolean2DArraysOperations.copy(board);
-    }
-
-    public Vector2 getExitCell() {
-        return exitCell;
-    }
-
-    @Override
-    public boolean isMoveValid(Player player, Direction direction) {
-        return !isGoingOutside(player, direction) &&
-                !isGoingIntoWall(player, direction);
-    }
-
-    /**
-     * Checks if the given player will end up outside of the labyrinth
-     * if he makes the move with the given direction
-     * 
-     * Here the board is represented as a horizontal segment
-     * so it is possible to move horizontally
-     * but moving vertically isn't because there are borders which are walls
-     * 
-     * @param player
-     * @param direction
-     * @return true if the move will move the player outside;
-     *         otherwise false
-     */
-    private boolean isGoingOutside(Player player, Direction direction) {
-        switch (direction) {
-            case UP:
-                return player.getY() <= 0;
-            case DOWN:
-                return player.getY() >= board.length - 1;
-            case LEFT:
-                return player.getX() <= 0;
-            case RIGHT:
-                return player.getX() >= board[1].length - 1;
-            default:
-                return false;
-        }
-    }
-
-    /**
-     * Checks if the given player will end up in a wall
-     * if he makes the move with the given direction
-     * 
-     * Here the board is represented as a horizontal segment
-     * so it is possible to move horizontally
-     * but moving vertically isn't because there are borders which are walls
-     * 
-     * @param player
-     * @param direction
-     * @return true if newPosition is a wall;
-     *         false otherwise
-     */
-    private boolean isGoingIntoWall(Player player, Direction direction)
-            throws IllegalArgumentException {
-        int newX = player.getX();
-        int newY = player.getY();
-        switch (direction) {
-            case UP:
-            case DOWN:
-                newY = player.getY() + direction.getStep();
-                break;
-            case LEFT:
-            case RIGHT:
-                newX = player.getX() + direction.getStep();
-                break;
-            default:
-                throw new IllegalArgumentException("Direction " + direction + " is not supported");
-        }
-        return isWall(newX, newY);
-    }
-
-    /**
-     * @param x
-     * @param y
-     * @return true if there is a wall at the given coordinates;
-     *         false otherwise
-     */
-    private boolean isWall(int x, int y) {
-        return !board[y][x];
-    }
-
-    @Override
-    public void movePlayer(Player player, Direction direction) {
-        if (isMoveValid(player, direction)) {
-            player.move(direction);
-        }
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.gytmy.labyrinth.LabyrinthModel#isGameOver()
-     * 
-     * Here, the game is considered over when
-     * all the players have made it to the exit
-     * 
-     * May be modified to fit the definition of a game
-     */
-    @Override
-    public boolean isGameOver() {
-        if (players == null || players.length == 0) {
-            return true;
-        }
-
-        for (Player player : players) {
-            if (!isPlayerAtExit(player))
-                return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean isPlayerAtExit(Player player) {
-        return player.getCoordinates().equals(exitCell);
-    }
+  @Override
+  public boolean isPlayerAtExit(Player player) {
+    return player.getCoordinates().equals(exitCell);
+  }
 
 }
