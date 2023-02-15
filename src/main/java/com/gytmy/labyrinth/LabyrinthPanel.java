a3000beff824c934ea2b87cfca8f49213671d99d
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-02-15 23:58:45 +0100

    Show initialCell and exitCell on view

--- src/main/java/com/gytmy/labyrinth/LabyrinthPanel.java
+++ src/main/java/com/gytmy/labyrinth/LabyrinthPanel.java
@@ -6,29 +6,33 @@ import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
+import com.gytmy.utils.Coordinates;
+
 public class LabyrinthPanel extends JPanel {
 
+    private LabyrinthModel model;
     private boolean[][] board;
     private int nbRowsBoard;
     private int nbColsBoard;
-    private static final int CELL_SIZE = 20;
+    private static final int CELL_SIZE = 24;
 
     public LabyrinthPanel(LabyrinthModel model) {
+        this.model = model;
         board = model.getBoard();
         nbRowsBoard = board.length;
         nbColsBoard = board[0].length;
 
-        updateLabyrinthPanel();
+        initLabyrinthPanel();
         setPreferredSize(new Dimension(nbColsBoard * CELL_SIZE, nbRowsBoard * CELL_SIZE));
     }
 
-    public void updateLabyrinthPanel() {
+    public void initLabyrinthPanel() {
         GridLayout grid = new GridLayout(nbRowsBoard, nbColsBoard);
         setLayout(grid);
-        updateLabyrinthCells();
+        updateAllLabyrinthCells();
     }
 
-    private void updateLabyrinthCells() {
+    private void updateAllLabyrinthCells() {
         for (int row = 0; row < nbRowsBoard; row++) {
             for (int col = 0; col < nbColsBoard; col++) {
                 updateCell(row, col);
@@ -40,11 +44,22 @@ public class LabyrinthPanel extends JPanel {
         GridLayout cellGrid = new GridLayout(2, 2);
         JPanel cell = new JPanel(cellGrid);
         cell.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
-        if (board[row][col]) {
-            cell.setBackground(Color.WHITE);
+        Coordinates cellToUpdate = new Coordinates(col, row);
+        Coordinates initialCell = model.getInitialCell();
+        Coordinates exitCell = model.getExitCell();
+
+        if (cellToUpdate.equals(initialCell)) {
+            cell.setBackground(Color.GREEN);
+        } else if (cellToUpdate.equals(exitCell)) {
+            cell.setBackground(Color.RED);
         } else {
-            cell.setBackground(Color.BLACK);
+            if (board[row][col]) {
+                cell.setBackground(Color.WHITE);
+            } else {
+                cell.setBackground(Color.BLACK);
+            }
         }
+
         add(cell);
     }
 
