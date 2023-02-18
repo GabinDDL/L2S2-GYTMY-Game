031c4c9dca1d19f20ef477d8e9582547c4aaf473
Yanis <yanis.lacenne@gmail.com>
2023-02-18 18:45:34 +0100

    Asserts User Folder is well Built

--- src/main/java/com/gytmy/sound/AudioToFile.java
+++ src/main/java/com/gytmy/sound/AudioToFile.java
@@ -1,9 +1,15 @@
 package com.gytmy.sound;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import com.gytmy.utils.WordsToRecord;
 
 public class AudioToFile {
 
+    /**
+     * Starts recording an audio in which the user says the word recorded
+     */
     public static void record(User user, String wordRecorded) {
 
         try {
@@ -12,6 +18,7 @@ public class AudioToFile {
         }
 
         assertWordRecordedIsValid(wordRecorded);
+        assertUserFolderIsValid(user, wordRecorded);
 
         int numberOfRecordings = AudioFileManager.numberOfRecordings(user.getFirstname(), wordRecorded) + 1;
 
@@ -20,9 +27,27 @@ public class AudioToFile {
         new SoundRecorder(path).start();
     }
 
+    /**
+     * Asserts that the word recorded is a word we want to record
+     * 
+     * @param wordRecorded
+     */
     private static void assertWordRecordedIsValid(String wordRecorded) {
         if (!WordsToRecord.exists(wordRecorded)) {
             throw new IllegalArgumentException("Invalid word recorded");
+        }
+    }
+
+    /**
+     * Asserts that the user folder contains the word recorded folder
+     */
+    private static void assertUserFolderIsValid(User user, String wordRecorded) {
+
+        File userDirectory = new File(user.userAudioFilePath());
+        ArrayList<File> userFiles = AudioFileManager.getFilesVerifyingPredicate(userDirectory, File::isDirectory);
+
+        if (!userFiles.contains(new File(user.userAudioFilePath() + wordRecorded))) {
+            new File(user.userAudioFilePath() + wordRecorded).mkdir();
         }
     }
 }
