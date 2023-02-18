ae156de28b4781350d727de16161092c16907024
Yanis <yanis.lacenne@gmail.com>
2023-02-18 19:23:34 +0100

    Handle Exception within methods Read and Write

--- src/main/java/com/gytmy/sound/YamlReader.java
+++ src/main/java/com/gytmy/sound/YamlReader.java
@@ -13,25 +13,32 @@ public class YamlReader {
      * @return ApplicationConfig associatesd
      * @throws Exception
      */
-    public static User read(String filePath) throws Exception {
+    public static User read(String filePath) {
         handleInvalidFilePath(filePath, true);
 
         File file = new File(filePath);
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
-        User user = objectMapper.readValue(file,
-                User.class);
-        return user;
+        try {
+            User user = objectMapper.readValue(file, User.class);
+            return user;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Could not read file : " + filePath);
+        }
     }
 
     /**
      * Write the .yaml config file the user
      */
-    public static void write(String filePath, User user, boolean shouldExists) throws Exception {
+    public static void write(String filePath, User user, boolean shouldExists) {
         handleInvalidFilePath(filePath, shouldExists);
 
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        objectMapper.writeValue(new File(filePath), user);
+        try {
+            objectMapper.writeValue(new File(filePath), user);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Could not write to file : " + filePath);
+        }
     }
 
     /**
