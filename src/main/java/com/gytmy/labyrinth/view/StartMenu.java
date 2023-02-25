79f014430eac57f6578977de1cea176ca3985b48
LACENNE yanis <yanis.lacenne@gmail.com>
2023-02-25 18:41:34 +0100

    Draw background image on main frame

--- src/main/java/com/gytmy/labyrinth/view/StartMenu.java
+++ src/main/java/com/gytmy/labyrinth/view/StartMenu.java
@@ -1,17 +1,42 @@
 package com.gytmy.labyrinth.view;
 
+import java.awt.BorderLayout;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class StartMenu extends JPanel {
     private JFrame frame;
 
+    private static final String BACKGROUND_IMAGE = "src/resources/images/MazeMenu.jpg";
+    private JPanel menu;
+
     public StartMenu(JFrame frame) {
         this.frame = frame;
+
+        setLayout(new BorderLayout());
 
         initMenu();
     }
 
     private void initMenu() {
+        menu = new JPanel(new BorderLayout());
+
+        initImageBackground();
+
+        add(menu, BorderLayout.CENTER);
+    }
+
+    private void initImageBackground() {
+        JPanel background = null;
+        try {
+            background = new JPanelWithBackground(BACKGROUND_IMAGE);
+        } catch (IOException ioe) {
+            System.out.println("Error while loading the background image");
+            System.out.println(ioe.getMessage());
+            ioe.getStackTrace();
+        }
+        menu.add(background);
     }
 }
