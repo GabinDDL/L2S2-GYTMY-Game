2be21098bffc938efc93678bf126a1dbf369dba5
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-26 11:15:31 +0100

    Document addKeyController

--- src/main/java/com/gytmy/labyrinth/view/LabyrinthView.java
+++ src/main/java/com/gytmy/labyrinth/view/LabyrinthView.java
@@ -15,9 +15,13 @@ public abstract class LabyrinthView extends JPanel {
     public abstract LabyrinthPanel getLabyrinthPanel();
 
     public void addKeyController(KeyListener keyController) {
-        //
+
+        // The EventQueue is used to ensure that the key listener is added after the
+        // component is added to the frame. If it is added before, the component will
+        // only receive key events after it is clicked.
         EventQueue.invokeLater(() -> {
             addKeyListener(keyController);
+            // In order to be able to receive key events, the component must be focusable.
             setFocusable(true);
             requestFocusInWindow();
         });
