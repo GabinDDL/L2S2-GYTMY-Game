01fe18d0e83ececf085672ffcd3e09af87966006
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-27 14:23:37 +0100

    Refactoring of the countdown

--- src/main/java/com/gytmy/labyrinth/view/TimerPanel.java
+++ src/main/java/com/gytmy/labyrinth/view/TimerPanel.java
@@ -12,36 +12,43 @@ import javax.swing.Timer;
 public class TimerPanel extends JPanel implements ActionListener {
     private JLabel timerLabel;
     private Timer timer;
-    private int counter;
+
+    private int counter = 0;
     private int countdown = 3;
 
     private static final Color BACKGROUND_COLOR = Cell.WALL_COLOR;
     private static final Color COUNTDOWN_COLOR = Color.decode("#EE8695");
     private static final Color FOREGROUND_COLOR = Cell.PATH_COLOR;
+
     private static final Font FONT = new Font("Arial", Font.BOLD, 20);
 
     public TimerPanel() {
         timerLabel = new JLabel("[ 00:03 ]");
-        timerLabel.setForeground(COUNTDOWN_COLOR);
         timerLabel.setFont(FONT);
+        timerLabel.setForeground(COUNTDOWN_COLOR);
         setBackground(BACKGROUND_COLOR);
         add(timerLabel);
 
-        timer = new Timer(1000, this);
-        counter = 0;
+        timer = new Timer(1000, event -> {
+            if (countdown > 0) {
+                countdown--;
+                timerLabel.setText(getStringTime(countdown));
+
+            } else {
+                timer.stop();
+                timerLabel.setForeground(FOREGROUND_COLOR);
+                timerLabel.setText(getStringTime(counter));
+                timer = new Timer(1000, this);
+                timer.start();
+            }
+        });
+
     }
 
     public void actionPerformed(ActionEvent e) {
+        counter++;
+        timerLabel.setText(getStringTime(counter));
 
-        if (countdown > 0) {
-            countdown--;
-            timerLabel.setText(getStringTime(countdown));
-        } else {
-            counter++;
-            System.out.println(counter);
-            timerLabel.setForeground(FOREGROUND_COLOR);
-            timerLabel.setText(getStringTime(counter));
-        }
     }
 
     private String getStringTime(int counter) {
