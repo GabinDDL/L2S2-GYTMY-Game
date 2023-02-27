62bf9b175556f1a66c4487d7c4f485aae24e3962
LACENNE yanis <yanis.lacenne@gmail.com>
2023-02-27 12:47:29 +0100

    GoBack Button

--- src/main/java/com/gytmy/labyrinth/view/OptionsMenu.java
+++ src/main/java/com/gytmy/labyrinth/view/OptionsMenu.java
@@ -85,6 +85,7 @@ public class OptionsMenu extends JPanel {
         audioPanel.add(recordButton);
 
         JButton goBackButton = new JButton("Go back");
+        goBackButton.addActionListener(e -> goBackToStartMenu());
         audioPanel.add(goBackButton);
 
         add(audioPanel, BorderLayout.EAST);
@@ -100,5 +101,8 @@ public class OptionsMenu extends JPanel {
     public void goBackToStartMenu() {
         frame.setContentPane(new StartMenu(frame));
         GameFrameToolbox.frameUpdate(frame, "Menu");
+        frame.setLocationRelativeTo(null);
+        frame.setSize(800, 500);
+        frame.revalidate();
     }
 }
