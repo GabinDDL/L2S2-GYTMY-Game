30c354269bf3e3ea96942110c3592b9dc71409bb
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-02-27 15:20:45 +0100

    Add missing @Override

--- src/main/java/com/gytmy/labyrinth/view/TimerPanel.java
+++ src/main/java/com/gytmy/labyrinth/view/TimerPanel.java
@@ -70,6 +70,7 @@ public class TimerPanel extends JPanel implements ActionListener {
 
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         counterInSeconds++;
         timerLabel.setText(getStringTime(counterInSeconds));
