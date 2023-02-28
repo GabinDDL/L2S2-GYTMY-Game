1e3046f81556f3b6c4497b812ea4851b321deb95
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-01 00:41:23 +0100

    Fast testing GUI

--- src/main/java/com/gytmy/Main.java
+++ src/main/java/com/gytmy/Main.java
@@ -1,13 +1,43 @@
 package com.gytmy;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+
+import com.gytmy.labyrinth.controller.LabyrinthController;
+import com.gytmy.labyrinth.controller.LabyrinthControllerImplementation;
+import com.gytmy.labyrinth.model.GameData;
+import com.gytmy.labyrinth.model.player.Player;
+import com.gytmy.labyrinth.model.player.PlayerImplementation;
+import com.gytmy.labyrinth.view.LabyrinthView;
 import com.gytmy.launcher.GraphicalLauncher;
+import com.gytmy.utils.Coordinates;
 
 public class Main {
 
     public static void main(String[] args) {
+
         EventQueue.invokeLater(new GraphicalLauncher());
+
+        // Player player1 = new PlayerImplementation(new Coordinates(1, 1), "Grep",
+        // Color.BLACK, true);
+
+        // int length = 10;
+        // int height = 10;
+
+        // GameData data = new GameData(length, height, new Player[] { player1 });
+        // LabyrinthController labyrinthController = new
+        // LabyrinthControllerImplementation(data);
+        // LabyrinthView labyrinthView = labyrinthController.getView();
+
+        // JFrame frame = new JFrame("Labyrinth");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.add(labyrinthView);
+        // frame.pack();
+        // frame.setLocationRelativeTo(null);
+        // frame.setVisible(true);
+
     }
 
 }
