2af86c5bd4ef9fe711f9f880ba8abade30cc66b8
LACENNE yanis <yanis.lacenne@gmail.com>
2023-02-28 17:53:13 +0100

    Change to lambda expression in FileTree

--- src/main/java/com/gytmy/utils/FileTree.java
+++ src/main/java/com/gytmy/utils/FileTree.java
@@ -1,7 +1,6 @@
 package com.gytmy.utils;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.io.File;
 
 import javax.swing.JLabel;
@@ -38,34 +37,27 @@ public class FileTree extends JTree {
         setScrollsOnExpand(true);
         setShowsRootHandles(true);
 
-        addTreeSelectionListener(e -> {
-            repaint();
-        });
+        addTreeSelectionListener(e -> repaint());
 
         setCellRenderer(getCellRenderer());
     }
 
     @Override
     public TreeCellRenderer getCellRenderer() {
-        return new TreeCellRenderer() {
+        return (tree, value, selected, expanded, leaf, row, hasFocus) -> {
 
-            @Override
-            public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,
-                    boolean leaf, int row, boolean hasFocus) {
+            JLabel label = new JLabel(((File) value).getName());
+            label.setOpaque(true);
 
-                JLabel label = new JLabel(((File) value).getName());
-                label.setOpaque(true);
-
-                if (selected) {
-                    label.setBackground(SELECTION_COLOR);
-                    label.setForeground(BACKGROUND_COLOR);
-                } else {
-                    label.setBackground(BACKGROUND_COLOR);
-                    label.setForeground(FOREGROUND_COLOR);
-                }
-
-                return label;
+            if (selected) {
+                label.setBackground(SELECTION_COLOR);
+                label.setForeground(BACKGROUND_COLOR);
+            } else {
+                label.setBackground(BACKGROUND_COLOR);
+                label.setForeground(FOREGROUND_COLOR);
             }
+
+            return label;
         };
     }
 }
