5daa962fa4252a8f484c4e0da9efb917659f8247
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-03-02 18:30:54 +0100

    Changed : Tabulations using spaces instead of tabs

--- src/main/java/com/gytmy/sound/AudioPlayer.java
+++ src/main/java/com/gytmy/sound/AudioPlayer.java
@@ -21,98 +21,98 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public class AudioPlayer implements LineListener {
 
-	/**
-	 * this flag indicates whether the playback completes or not.
-	 */
-	private boolean playing;
+    /**
+     * this flag indicates whether the playback completes or not.
+     */
+    private boolean playing;
 
-	/**
-	 * this flag indicates whether the playback is stopped or not.
-	 */
-	private boolean isStopped;
+    /**
+     * this flag indicates whether the playback is stopped or not.
+     */
+    private boolean isStopped;
 
-	private Clip audioClip;
+    private Clip audioClip;
 
-	/**
-	 * Load audio file before playing back
-	 * 
-	 * @param audioFilePath
-	 *                      Path of the audio file.
-	 * @throws IOException
-	 * @throws UnsupportedAudioFileException
-	 * @throws LineUnavailableException
-	 */
-	public void load(String audioFilePath)
-			throws UnsupportedAudioFileException, IOException,
-			LineUnavailableException {
-		File audioFile = new File(audioFilePath);
+    /**
+     * Load audio file before playing back
+     * 
+     * @param audioFilePath
+     *                      Path of the audio file.
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws LineUnavailableException
+     */
+    public void load(String audioFilePath)
+            throws UnsupportedAudioFileException, IOException,
+            LineUnavailableException {
+        File audioFile = new File(audioFilePath);
 
-		AudioInputStream audioStream = AudioSystem
-				.getAudioInputStream(audioFile);
+        AudioInputStream audioStream = AudioSystem
+                .getAudioInputStream(audioFile);
 
-		AudioFormat format = audioStream.getFormat();
+        AudioFormat format = audioStream.getFormat();
 
-		DataLine.Info info = new DataLine.Info(Clip.class, format);
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
 
-		audioClip = (Clip) AudioSystem.getLine(info);
+        audioClip = (Clip) AudioSystem.getLine(info);
 
-		audioClip.addLineListener(this);
+        audioClip.addLineListener(this);
 
-		audioClip.open(audioStream);
-	}
+        audioClip.open(audioStream);
+    }
 
-	public long getClipSecondLength() {
-		return audioClip.getMicrosecondLength() / 1_000_000;
-	}
+    public long getClipSecondLength() {
+        return audioClip.getMicrosecondLength() / 1_000_000;
+    }
 
-	/**
-	 * Play a given audio file.
-	 * 
-	 * @throws IOException
-	 * @throws UnsupportedAudioFileException
-	 * @throws LineUnavailableException
-	 */
-	public void play() throws IOException {
+    /**
+     * Play a given audio file.
+     * 
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws LineUnavailableException
+     */
+    public void play() throws IOException {
 
-		audioClip.start();
+        audioClip.start();
 
-		playing = true;
-		isStopped = false;
+        playing = true;
+        isStopped = false;
 
-		while (playing) {
-			// wait for the playback completes
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException ex) {
-				if (isStopped) {
-					audioClip.stop();
-					break;
-				}
-			}
-		}
-		audioClip.close();
+        while (playing) {
+            // wait for the playback completes
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                if (isStopped) {
+                    audioClip.stop();
+                    break;
+                }
+            }
+        }
+        audioClip.close();
 
-	}
+    }
 
-	/**
-	 * Stop playing back.
-	 */
-	public void stop() {
-		isStopped = true;
-	}
+    /**
+     * Stop playing back.
+     */
+    public void stop() {
+        isStopped = true;
+    }
 
-	/**
-	 * Listens to the audio line events to know when the playback completes.
-	 */
-	@Override
-	public void update(LineEvent event) {
-		LineEvent.Type type = event.getType();
-		if (type == LineEvent.Type.STOP) {
-			playing = false;
-		}
-	}
+    /**
+     * Listens to the audio line events to know when the playback completes.
+     */
+    @Override
+    public void update(LineEvent event) {
+        LineEvent.Type type = event.getType();
+        if (type == LineEvent.Type.STOP) {
+            playing = false;
+        }
+    }
 
-	public Clip getAudioClip() {
-		return audioClip;
-	}
+    public Clip getAudioClip() {
+        return audioClip;
+    }
 }
--- src/main/java/com/gytmy/sound/PlayingTimer.java
+++ src/main/java/com/gytmy/sound/PlayingTimer.java
@@ -17,67 +17,67 @@ import javax.swing.JProgressBar;
  *
  */
 public class PlayingTimer extends Thread {
-	private DateFormat dateFormater = new SimpleDateFormat("mm:ss");
-	private boolean isRunning = false;
-	private boolean isReset = false;
-	private long startTime;
-	private long pauseTime;
+    private DateFormat dateFormater = new SimpleDateFormat("mm:ss");
+    private boolean isRunning = false;
+    private boolean isReset = false;
+    private long startTime;
+    private long pauseTime;
 
-	private JLabel labelRecordTime;
-	private JProgressBar timeProgress;
-	private Clip audioClip;
+    private JLabel labelRecordTime;
+    private JProgressBar timeProgress;
+    private Clip audioClip;
 
-	public void setAudioClip(Clip audioClip) {
-		this.audioClip = audioClip;
-	}
+    public void setAudioClip(Clip audioClip) {
+        this.audioClip = audioClip;
+    }
 
-	public PlayingTimer(JLabel labelRecordTime, JProgressBar timeProgress) {
-		this.labelRecordTime = labelRecordTime;
-		this.timeProgress = timeProgress;
-	}
+    public PlayingTimer(JLabel labelRecordTime, JProgressBar timeProgress) {
+        this.labelRecordTime = labelRecordTime;
+        this.timeProgress = timeProgress;
+    }
 
-	public void run() {
-		isRunning = true;
+    public void run() {
+        isRunning = true;
 
-		startTime = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
 
-		while (isRunning) {
-			try {
-				Thread.sleep(100);
-				if (audioClip != null && audioClip.isRunning()) {
-					labelRecordTime.setText(toTimeString());
-					int currentSecond = (int) audioClip.getMicrosecondPosition() / 1_000_000;
-					timeProgress.setValue(currentSecond);
-				}
-			} catch (InterruptedException ex) {
-				if (isReset) {
-					timeProgress.setValue(0);
-					labelRecordTime.setText("00:00");
-					isRunning = false;
-					break;
-				}
-			}
-		}
-	}
+        while (isRunning) {
+            try {
+                Thread.sleep(100);
+                if (audioClip != null && audioClip.isRunning()) {
+                    labelRecordTime.setText(toTimeString());
+                    int currentSecond = (int) audioClip.getMicrosecondPosition() / 1_000_000;
+                    timeProgress.setValue(currentSecond);
+                }
+            } catch (InterruptedException ex) {
+                if (isReset) {
+                    timeProgress.setValue(0);
+                    labelRecordTime.setText("00:00");
+                    isRunning = false;
+                    break;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Reset counting to "00:00"
-	 */
-	public void reset() {
-		isReset = true;
-		isRunning = false;
-	}
+    /**
+     * Reset counting to "00:00"
+     */
+    public void reset() {
+        isReset = true;
+        isRunning = false;
+    }
 
-	/**
-	 * Generate a String for time counter in the format of "HH:mm:ss"
-	 * 
-	 * @return the time counter
-	 */
-	private String toTimeString() {
-		long now = System.currentTimeMillis();
-		Date current = new Date(now - startTime - pauseTime);
-		dateFormater.setTimeZone(TimeZone.getTimeZone("GMT"));
-		String timeCounter = dateFormater.format(current);
-		return timeCounter;
-	}
+    /**
+     * Generate a String for time counter in the format of "HH:mm:ss"
+     * 
+     * @return the time counter
+     */
+    private String toTimeString() {
+        long now = System.currentTimeMillis();
+        Date current = new Date(now - startTime - pauseTime);
+        dateFormater.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String timeCounter = dateFormater.format(current);
+        return timeCounter;
+    }
 }
