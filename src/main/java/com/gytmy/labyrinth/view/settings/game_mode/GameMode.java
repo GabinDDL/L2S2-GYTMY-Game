037843779cc3361322603627f3ca92d7b7ec978e
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-03 22:45:31 +0100

    Update the enum to handle personalized strings for the values

--- src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameMode.java
+++ src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameMode.java
@@ -1,5 +1,15 @@
 package com.gytmy.labyrinth.view.settings.game_mode;
 
 public enum GameMode {
-    CLASSIC,
+    CLASSIC("Classic Mode");
+
+    private final String displayName;
+
+    private GameMode(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
