437af25daab0f141745eb67c934d2c9df773cdd8
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-04 12:32:16 +0100

    refactor and document GameModeSelectionPanel

--- src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameModeSelectionPanel.java
+++ src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameModeSelectionPanel.java
@@ -10,7 +10,6 @@ import javax.swing.JPanel;
 
 import com.gytmy.labyrinth.view.Cell;
 
-//TODO: Refacctor this
 public class GameModeSelectionPanel extends JPanel {
 
     public static final Color BACKGROUND_COLOR = Cell.WALL_COLOR;
@@ -29,34 +28,43 @@ public class GameModeSelectionPanel extends JPanel {
 
     private void initGameModeSelector() {
         gameModeSelector = new GameModeSelector();
+        gameModeSelector.setBackground(BACKGROUND_COLOR);
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.PAGE_START;
-        gbc.weightx = 1.0;
+        gbc.weightx = 1;
         gbc.weighty = 0.3;
         add(gameModeSelector, gbc);
-
-        revalidate();
-        repaint();
     }
 
     private void initGameModeSettingsPanel() {
         gameModeSettingsPanel = new JPanel();
-        gameModeSelector.setBackground(BACKGROUND_COLOR);
+        gameModeSettingsPanel.setBackground(BACKGROUND_COLOR);
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 1;
         gbc.weighty = 0.7;
-        gameModeSelector.updateGameModeSettingsPanel((GameMode) gameModeSelector.getSelectedItem());
-        gameModeSettingsPanel.setBackground(BACKGROUND_COLOR);
         add(gameModeSettingsPanel, gbc);
 
-        revalidate();
-        repaint();
+        firstGameModeSettingsPanelUpdate();
+    }
+
+    /**
+     * We need to call this method only once, because we want to update the panel
+     * with the first game mode. The issue is that the panel should be updated
+     * only after the panel is added to the frame. If we call this method in the
+     * constructor, the panel will be updated before it is added to the frame.
+     * That's why we need to call this method only once, after the panel is added
+     * to the frame.
+     */
+    private void firstGameModeSettingsPanelUpdate() {
+        gameModeSelector.updateGameModeSettingsPanel((GameMode) gameModeSelector.getSelectedItem());
     }
 
     public GameModeData getGameModeData() {
@@ -69,13 +77,22 @@ public class GameModeSelectionPanel extends JPanel {
         return (GameMode) gameModeSelector.getSelectedItem();
     }
 
+    /**
+     * This inner class represents a combo box with all available game modes. When
+     * the user selects a game mode, the panel with the settings for this game mode
+     * is updated. For this we use a factory that returns the appropriate handler
+     * for the selected game mode. The handler is responsible for updating the panel
+     * with the settings for the selected game mode. And it is also responsible for
+     * cleaning the panel when the user selects another game mode.
+     */
     private class GameModeSelector extends JComboBox<GameMode> {
+        // We need to remember the last selected game mode, because we need to
+        // clean the panel with the settings for this game mode.
         private GameMode lastSelectedGameMode;
 
-        private final Font FONT = new Font("Arial", Font.BOLD, 15);
-
         public GameModeSelector() {
-            setFont(FONT);
+            Font font = new Font("Arial", Font.BOLD, 15);
+            setFont(font);
 
             for (GameMode gameMode : GameMode.values()) {
                 addItem(gameMode);
@@ -92,6 +109,7 @@ public class GameModeSelectionPanel extends JPanel {
 
         private void updateGameModeSettingsPanel(GameMode gameMode) {
             cleanOldPanel();
+
             lastSelectedGameMode = (GameMode) getSelectedItem();
             GameModeSettingsPanelHandler handler = GameModeSettingsPanelHandlerFactory
                     .getGameModeSettingsPanel(gameMode);
