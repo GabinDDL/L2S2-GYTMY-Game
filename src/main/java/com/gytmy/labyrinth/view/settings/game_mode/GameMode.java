9742c0dfa7166e0d8c838fad797c4611032bb95c
IGLESIAS VAZQUEZ Yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-04 14:21:48 +0100

    Document GameMode

--- src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameMode.java
+++ src/main/java/com/gytmy/labyrinth/view/settings/game_mode/GameMode.java
@@ -1,6 +1,27 @@
 package com.gytmy.labyrinth.view.settings.game_mode;
 
-//TODO: Document this enum
+/**
+ * This enum represents the game modes. It is used to select the game mode
+ * and to get the settings data of the game mode. It also allows to get the
+ * display name of the game mode.
+ * 
+ * In order to add a new game mode, you need to follow these steps:
+ * <ol>
+ * <li>Add a new enum value</li>
+ * <li>Add a new {@code GameModeSettingsPanelHandler} implementation</li>
+ * <li>Add a new {@code GameModeSettingsPanelHandler} instance to the
+ * {@code GameModeSettingsPanelHandlerFactory}</li>
+ * <li>Add a new {@code GameModeData} implementation</li>
+ * <li>Add a new {@code LabyrinthModel} creation method on the switch case
+ * inside {@code LabyrinthModelFactory}
+ * </li>
+ * </ol>
+ * 
+ * @see com.gytmy.labyrinth.view.settings.game_mode.GameModeSettingsPanelHandler
+ * @see com.gytmy.labyrinth.view.settings.game_mode.GameModeSettingsPanelHandlerFactory
+ * @see com.gytmy.labyrinth.view.settings.game_mode.GameModeData
+ * @see com.gytmy.labyrinth.model.LabyrinthModelFactory
+ */
 public enum GameMode {
     CLASSIC("Classic Mode"),
     ONE_DIMENSION("One-Dimensional Mode");
