1229782ff1ab3899d4d653e00ee7251bb5de3376
gdudilli <gdudillieu@gmail.com>
2023-03-14 00:13:17 +0100

    add exit on run

--- src/main/java/com/gytmy/utils/input/RunSH.java
+++ src/main/java/com/gytmy/utils/input/RunSH.java
@@ -5,16 +5,18 @@ import java.util.Arrays;
 
 public class RunSH {
 
-    public static void run(String path, String[] args) {
+    public static int run(String path, String[] args) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(path);
             processBuilder.command().addAll(Arrays.asList(args));
             Process process = processBuilder.start();
 
             int exitCode = process.waitFor();
+
+            return exitCode;
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
+        return 1;
     }
 }
