97e81e1926dcfd06e3b7f7d5686c2bdb8a22dd2b
gdudilli <gdudillieu@gmail.com>
2023-03-19 15:21:33 +0100

    Fixed TestAudioFileManager Problem

--- src/test/java/com/gytmy/sound/TestAudioFileManager.java
+++ src/test/java/com/gytmy/sound/TestAudioFileManager.java
@@ -82,8 +82,8 @@ public class TestAudioFileManager {
 
         int totalAudioFiles = AudioFileManager.totalNumberOfAudioFiles();
 
-        File up = new File(user.audioFilesPath(), "HAUT");
-        File down = new File(user.audioFilesPath(), "BAS");
+        File up = new File(user.audioPath(), "HAUT");
+        File down = new File(user.audioPath(), "BAS");
         up.mkdir();
         down.mkdir();
 
