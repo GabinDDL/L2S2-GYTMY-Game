0978c9926acb3add631703dc9c8049a31e7c5813
gdudilli <gdudillieu@gmail.com>
2023-03-21 00:16:44 +0100

    update test of sh

--- src/main/java/com/gytmy/utils/TestRunSh.sh
+++ src/main/java/com/gytmy/utils/TestRunSh.sh
@@ -1,10 +1,12 @@
-if [ $1 = "exit0" ]; then
+#!/bin/bash
+
+if [ "$1" == "exit0" ]; then
 	exit 0
 fi
-if [ $1 = "exit1" ]; then
+if [ "$1" == "exit1" ]; then
 	exit 1
 fi
-if [ $2 = "exit2" ]; then
+if [ "$2" == "exit2" ]; then
 	exit 2
 fi
 exit 3
