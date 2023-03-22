9e4ec2dd252b7764d6e9863de343fa028f56d70d
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-03-22 14:49:18 +0100

    Removed unnecessary import

--- src/main/java/com/gytmy/labyrinth/view/GameOverPanel.java
+++ src/main/java/com/gytmy/labyrinth/view/GameOverPanel.java
@@ -7,7 +7,6 @@ import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
