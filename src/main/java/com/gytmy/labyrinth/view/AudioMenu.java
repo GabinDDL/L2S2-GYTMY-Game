0aef24e6e17a476ab415ae82a2d6388594f61d0b
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-23 21:20:03 +0100

    Use StringBuilder instead of string concatenation

--- src/main/java/com/gytmy/labyrinth/view/AudioMenu.java
+++ src/main/java/com/gytmy/labyrinth/view/AudioMenu.java
@@ -404,14 +404,14 @@ public class AudioMenu extends JPanel {
     }
 
     private String extractNumberFromWord(String string) {
-        String newString = "";
+        StringBuilder builder = new StringBuilder();
 
         for (int i = 0; i < string.length(); i++) {
             if (Character.isDigit(string.charAt(i))) {
-                newString += string.charAt(i);
+                builder.append(string.charAt(i));
             }
         }
-        return newString;
+        return builder.toString();
     }
 
     private void initLabelDuration(JComponent parentComponent) {
