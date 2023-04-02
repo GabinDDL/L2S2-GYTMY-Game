cb9710ff8b4956b509c732fc3cef94f1dead63c5
gdudilli <gdudillieu@gmail.com>
2023-04-02 17:07:08 +0200

    remove unused data of main

--- src/main/java/com/gytmy/Main.java
+++ src/main/java/com/gytmy/Main.java
@@ -3,18 +3,12 @@ package com.gytmy;
 import java.awt.EventQueue;
 
 import com.gytmy.launcher.GraphicalLauncher;
-import com.gytmy.sound.ModelManager;
-import com.gytmy.sound.User;
-import com.gytmy.sound.YamlReader;
 
 public class Main {
 
     public static void main(String[] args) {
 
         EventQueue.invokeLater(new GraphicalLauncher());
-
-        // User user = YamlReader.read("src/resources/audioFiles/YANIS/config.yaml");
-        // ModelManager.createModelOfRecordedWord(user, "BAS");
 
     }
 
