52baeba9bf947c55b08324952ff08f1299258319
gdudilli <gdudillieu@gmail.com>
2023-04-03 23:21:09 +0200

    Remove unused import

--- src/main/java/com/gytmy/utils/RunSH.java
+++ src/main/java/com/gytmy/utils/RunSH.java
@@ -3,11 +3,8 @@ package com.gytmy.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringJoiner;
 
 public class RunSH {
 
