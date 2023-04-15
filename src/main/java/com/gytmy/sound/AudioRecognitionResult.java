a87dfb7e50e993d4196f9ef82a885421fdd1ce28
gdudilli <gdudillieu@gmail.com>
2023-04-16 01:26:15 +0200

    Add AudioRecognitionResult class with init

---
+++ src/main/java/com/gytmy/sound/AudioRecognitionResult.java
@@ -0,0 +1,10 @@
+package com.gytmy.sound;
+
+public class AudioRecognitionResult {
+
+    private AudioRecognitionResult() {
+    }
+
+    public static final String EXE_PATH = "src/main/exe/comparaison/";
+    public static final String COMPUTE_TEST_SH_PATH = EXE_PATH + "ComputeTest.sh";
+}
