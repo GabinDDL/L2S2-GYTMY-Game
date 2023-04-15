a242e1ac3c3127ec3f2255b8200283bc20eeac54
gdudilli <gdudillieu@gmail.com>
2023-04-16 01:50:31 +0200

    Add function for ndx files

--- src/main/java/com/gytmy/sound/AudioRecognitionResult.java
+++ src/main/java/com/gytmy/sound/AudioRecognitionResult.java
@@ -1,5 +1,10 @@
 package com.gytmy.sound;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class AudioRecognitionResult {
 
     private AudioRecognitionResult() {
@@ -7,4 +12,65 @@ public class AudioRecognitionResult {
 
     public static final String EXE_PATH = "src/main/exe/comparaison/";
     public static final String COMPUTE_TEST_SH_PATH = EXE_PATH + "ComputeTest.sh";
+    public static final String NDX_LIST_PATH = EXE_PATH + "ndx/Liste.ndx";
+
+    public static final String CLIENT_MODEL_PATH = "src/ressources/audioFiles/client/model/";
+    public static final String CLIENT_RESULT_PATH = CLIENT_MODEL_PATH + "RecognitionResult.txt";
+
+    private static boolean tryToResetComputeTestNdxFile() {
+        try (FileWriter writer = new FileWriter(NDX_LIST_PATH, false);) {
+            writer.append("currentAudio");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean tryToUpdateComputeTestNdxFile() {
+        File dataDirectory = new File(ModelManager.GMM_PATH);
+
+        if (!dataDirectory.exists()) {
+            return false;
+        }
+        List<File> gmmList = AudioFileManager.getFilesVerifyingPredicate(dataDirectory,
+                AudioRecognitionResult::isGmmModelFile);
+
+        return tryToAddComputeTestNdxFile(gmmList);
+    }
+
+    public static boolean tryToAddComputeTestNdxFile(List<File> gmmList) {
+        try (FileWriter writer = new FileWriter(NDX_LIST_PATH, true);) {
+            for (File file : gmmList) {
+                writer.append(getFileBasename(file) + "\n");
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean isGmmModelFile(File file) {
+        return file.isFile() && file.getName().endsWith(".gmm") && !file.getName().startsWith("wld");
+    }
+
+    /**
+     * By convention, the name of a file is constitued of the basename
+     * and its extensions, _e.g._ basename.extension1.extension2...
+     *
+     * @example getFileBasename("test.wav") --> "test"
+     * @example getFileBasename("newTest.tar.gz") --> "newTest"
+     * @param file The file from which we want the basename
+     * @return The basename of the file
+     */
+    private static String getFileBasename(File file) {
+        String name = file.getName();
+        int index = file.getName().indexOf('.');
+
+        if (index == -1) {
+            return name;
+        }
+        return name.substring(0, index);
+    }
 }
