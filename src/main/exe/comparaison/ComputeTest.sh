5c8597d1bc7adcb4c71c042b5a95cd8b79e9c380
gdudilli <gdudillieu@gmail.com>
2023-04-16 19:40:03 +0200

    Change error ComputeTest changes

--- src/main/exe/comparaison/ComputeTest.sh
+++ src/main/exe/comparaison/ComputeTest.sh
@@ -3,6 +3,6 @@
 EXE_DIR="./src/main/exe/comparaison"
 CFG_DIR="./src/main/exe/comparaison/cfg"
 
-$EXE_DIR/EnergyDetector --config $CFG_DIR/EnergyDetector.cfg
+$EXE_DIR/ComputeTest --config $CFG_DIR/ComputeTest.cfg
 
-exit 0to
+exit 0
