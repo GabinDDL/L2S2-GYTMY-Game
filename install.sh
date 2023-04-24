7f74bd7d035036aa8c497621839434f6556b1a1b
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2023-04-24 14:51:11 +0200

    else logic corrected for virtual env creation

--- install.sh
+++ install.sh
@@ -9,16 +9,16 @@ function echo_color() {
     local text_type="$2"
     local message="$3"
     local line_break="$4"
-
+    
     local start=""
     local end="\e[0m"
-
+    
     case "$text_type" in
-    bold) start="\e[1m" ;;
-    underline) start="\e[4m" ;;
-    *) start="" ;;
+        bold) start="\e[1m" ;;
+        underline) start="\e[4m" ;;
+        *) start="" ;;
     esac
-
+    
     if [ "$line_break" = "b" ]; then
         echo -e "${start}\e[${color}m${message}${end}"
     else
@@ -86,26 +86,26 @@ fi
 
 # Installs Python 3.10 based on Linux (only Debian or Fedora)
 function install_python() {
-
+    
     confirm "Are you okay with installing Python $MAX_PYTHON_VERSION? (y/n)"
     read -r choice
-
+    
     # Check user's response
     case "$choice" in
-    y | Y)
-        confirmed "Okay, proceeding with installation of Python $MAX_PYTHON_VERSION."
+        y | Y)
+            confirmed "Okay, proceeding with installation of Python $MAX_PYTHON_VERSION."
         ;;
-    n | N)
-        confirmed "Python $MAX_PYTHON_VERSION will not be installed."
-        info "Packages may behave in unintended ways."
-        return
+        n | N)
+            confirmed "Python $MAX_PYTHON_VERSION will not be installed."
+            info "Packages may behave in unintended ways."
+            return
         ;;
-    *)
-        error "Invalid choice, please enter 'y' or 'n'."
-        return
+        *)
+            error "Invalid choice, please enter 'y' or 'n'."
+            return
         ;;
     esac
-
+    
     # Install Python 3.10
     if [ -f /etc/debian_version ]; then
         # Debian-based system
@@ -113,15 +113,15 @@ function install_python() {
         $SUDO apt-get update
         $SUDO apt-get install python3 -y
         $SUDO apt-get install python3-pip
-
-    elif [ -f /etc/fedora-release ]; then
+        
+        elif [ -f /etc/fedora-release ]; then
         # Fedora system
         info "Detected Fedora system."
         $SUDO dnf update
         $SUDO dnf install python3 -y
         $SUDO dnf install python3-pip
-
-    elif [ -f /etc/arch-release ]; then
+        
+        elif [ -f /etc/arch-release ]; then
         # Arch-based system
         info "Detected Arch-based system."
         $SUDO pacman -Syu
@@ -132,23 +132,23 @@ function install_python() {
         error "Unsupported operating system."
         exit 1
     fi
-
+    
     success "Python 3.10 installed successfully."
-
+    
     return
 }
 
 function make_python_ready() {
     # Install Python 3.10
     install_python
-
+    
     # check if virtualenv is installed
     if ! command -v virtualenv &>/dev/null; then
         warning "virtualenv is not installed."
-
+        
         # Install virtualenv
         pip3 install virtualenv &>/dev/null
-
+        
         # Check if virtualenv is installed
         if command -v virtualenv &>/dev/null; then
             confirmed "virtualenv installed successfully."
@@ -158,27 +158,27 @@ function make_python_ready() {
         fi
     else
         info "virtualenv is already installed."
-
-        # Check if virtual environment exists
-        if [ -d $ENV_NAME ]; then
-            warning "Virtual environment $ENV_NAME already exists."
-
-            # Check if virtual environment is activated
-            if [ "$VIRTUAL_ENV" = "$PWD/$ENV_NAME" ]; then
-                info "Virtual environment $ENV_NAME is already activated."
-            else
-                # Activate the virtual environment
-                source $ENV_NAME/bin/activate &>/dev/null
-            fi
+    fi
+    
+    # Check if virtual environment exists
+    if [ -d $ENV_NAME ]; then
+        warning "Virtual environment $ENV_NAME already exists."
+        
+        # Check if virtual environment is activated
+        if [ "$VIRTUAL_ENV" = "$PWD/$ENV_NAME" ]; then
+            info "Virtual environment $ENV_NAME is already activated."
         else
-            # Create a virtual environment using Python 3.10
-            virtualenv -p "/usr/bin/python3.10" $ENV_NAME &>/dev/null
-
             # Activate the virtual environment
             source $ENV_NAME/bin/activate &>/dev/null
         fi
+    else
+        # Create a virtual environment using Python 3.10
+        virtualenv -p "/usr/bin/python3.10" $ENV_NAME &>/dev/null
+        
+        # Activate the virtual environment
+        source $ENV_NAME/bin/activate &>/dev/null
     fi
-
+    
     # Check if Python version in virtual environment is within the specified range
     if python3 -c "import sys; exit(not (sys.version_info >= (3, 8) and sys.version_info < (3, 11)))"; then
         info "Python version in the virtual environment is between $MIN_PYTHON_VERSION and $MAX_PYTHON_VERSION."
@@ -186,24 +186,24 @@ function make_python_ready() {
         error "Failed to install Python 3.10 in virtual environment."
         exit 1
     fi
-
+    
     confirmed "Virtual environment $ENV_NAME with Python 3.10 activated successfully."
 }
 
 function make_pytorch_ready() {
-
+    
     # Activate the virtual environment
     source $ENV_NAME/bin/activate &>/dev/null
-
+    
     # Check if PyTorch is installed
     if ! python3 -c "import torch" &>/dev/null; then
         warning "PyTorch is not installed."
-
+        
         # Install PyTorch
         pip uninstall torch
         pip cache purge
         pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116I
-
+        
         # Check if PyTorch is installed
         if python3 -c "import torch" &>/dev/null; then
             confirmed "PyTorch installed successfully."
@@ -220,24 +220,24 @@ function make_ffmpeg_ready() {
     # Check if ffmpeg is installed
     if ! command -v ffmpeg &>/dev/null; then
         warning "ffmpeg is not installed."
-
+        
         # Install ffmpeg
         if [ -f /etc/debian_version ]; then
             # Debian-based system
             $SUDO apt-get install ffmpeg -y
-
-        elif [ -f /etc/fedora-release ]; then
+            
+            elif [ -f /etc/fedora-release ]; then
             # Fedora system
             $SUDO dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
             $SUDO dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
             $SUDO dnf install ffmpeg -y
-
+            
         else
             # Unsupported system
             echo "Unsupported operating system."
             exit 1
         fi
-
+        
         # Check if ffmpeg is installed
         if command -v ffmpeg &>/dev/null; then
             confirmed "ffmpeg installed successfully."
@@ -253,14 +253,14 @@ function make_ffmpeg_ready() {
 function make_whisper_ready() {
     # Activate the virtual environment
     source $ENV_NAME/bin/activate &>/dev/null
-
+    
     # Check if whisper is installed
     if ! command -v whisper-ctranslate2 &>/dev/null; then
         warning "Whisper is not installed."
-
+        
         # Install whisper
         pip3 install -U whisper-ctranslate2
-
+        
         # Check if whisper is installed
         if command -v whisper-ctranslate2 &>/dev/null; then
             confirmed "Whisper installed successfully."
