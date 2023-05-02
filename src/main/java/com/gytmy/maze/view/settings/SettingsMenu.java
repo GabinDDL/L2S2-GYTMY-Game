f65649b1f74aa88f63ac4a18104460040f2165ad
SOAN Tony Ly <tony-ly.soan@etu.u-paris.fr>
2023-05-02 22:42:49 +0200

    Removed print

--- src/main/java/com/gytmy/maze/view/settings/SettingsMenu.java
+++ src/main/java/com/gytmy/maze/view/settings/SettingsMenu.java
@@ -209,7 +209,6 @@ public class SettingsMenu extends JPanel {
     }
 
     private void updateUsers() {
-        System.out.println("Updating users");
         playerSelectionPanel.updateUsers();
     }
 }
