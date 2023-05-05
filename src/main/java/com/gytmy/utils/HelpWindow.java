4d6576cfbb731738cf0d0ac80d7ab38e8f105105
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-05 15:31:47 +0200

    Add ScrollBar

--- src/main/java/com/gytmy/utils/HelpWindow.java
+++ src/main/java/com/gytmy/utils/HelpWindow.java
@@ -1,20 +1,44 @@
 package com.gytmy.utils;
 
+import java.awt.Dimension;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class HelpWindow {
 
     private HelpWindow() {
     }
 
-    public static void showHelp(JComponent component, String windowTitle) {
+    public static void showHelp(JComponent component, String windowTitle, boolean scrollBar) {
         String message = getHelpMessage(windowTitle);
-        JOptionPane.showMessageDialog(component, message);
+
+        Object toDisplay;
+
+        if (scrollBar) {
+            toDisplay = createJScrollPane(message);
+        } else {
+            toDisplay = message;
+        }
+
+        JOptionPane.showMessageDialog(component, toDisplay);
+    }
+
+    private static Object createJScrollPane(String message) {
+        JTextArea textArea = new JTextArea(message);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(400, 300));
+
+        return scrollPane;
     }
 
     private static String getHelpMessage(String fileName) {
