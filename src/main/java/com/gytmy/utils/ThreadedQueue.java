7462337d6849848f8e3331cc0255bd56c827b986
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2023-05-09 07:28:15 +0200

    added delay for threading

--- src/main/java/com/gytmy/utils/ThreadedQueue.java
+++ src/main/java/com/gytmy/utils/ThreadedQueue.java
@@ -3,9 +3,9 @@ package com.gytmy.utils;
 import java.util.concurrent.*;
 
 public class ThreadedQueue {
-    private static final int MAX_THREADS = 30;
+    private static final int MAX_THREADS = 10;
     private static final int MAX_QUEUE_SIZE = 1000;
-
+    private static final int DELAY_MS = 100;
     private static final ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
     private static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);
 
@@ -22,6 +22,7 @@ public class ThreadedQueue {
             try {
                 Runnable task = queue.take();
                 executor.execute(task);
+                Thread.sleep(DELAY_MS);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
