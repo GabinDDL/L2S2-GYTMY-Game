cdfa804e127a512cbb4f4106c28792a501474482
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-05-09 19:01:06 +0200

    Remove useless import

--- src/main/java/com/gytmy/maze/view/EditCreateUsersPage.java
+++ src/main/java/com/gytmy/maze/view/EditCreateUsersPage.java
@@ -11,7 +11,6 @@ import java.awt.GridBagConstraints;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 
 import com.gytmy.maze.view.game.Cell;
 import com.gytmy.sound.AudioFileManager;
