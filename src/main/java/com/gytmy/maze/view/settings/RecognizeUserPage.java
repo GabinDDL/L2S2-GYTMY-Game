e1e648f38d8f382c640d14993287b0e9fd934886
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-11 02:30:12 +0200

    New version of RecognizUserPage

--- src/main/java/com/gytmy/maze/view/settings/RecognizeUserPage.java
+++ src/main/java/com/gytmy/maze/view/settings/RecognizeUserPage.java
@@ -1,12 +1,20 @@
 package com.gytmy.maze.view.settings;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.concurrent.CompletableFuture;
 
+import javax.swing.Box;
+import javax.swing.Icon;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import com.gytmy.maze.model.player.Player;
+import com.gytmy.maze.view.TimerPanel;
 import com.gytmy.maze.view.game.Cell;
 import com.gytmy.sound.AudioFileManager;
 import com.gytmy.sound.AudioRecognitionResult;
@@ -14,18 +22,37 @@ import com.gytmy.sound.AudioRecorder;
 import com.gytmy.sound.RecordObserver;
 import com.gytmy.sound.User;
 import com.gytmy.sound.AlizeRecognitionResultParser.AlizeRecognitionResult;
+import com.gytmy.utils.ImageManipulator;
 
 public class RecognizeUserPage extends JPanel implements RecordObserver {
 
     private JPanel playerPanel;
     private JLabel playerName;
     private JLabel recordStatus;
+    private TimerPanel timerPanel;
+    private JLabel triesStatus;
+
+    private User choosenUser;
+
+    private static final String TRIES_STATUS_TEXT = "Tries left: ";
+    private int maximumRecognitionTries = 3;
 
     private boolean isRecordingEnabled = false;
 
     private static final Color BACKGROUND_COLOR = Cell.WALL_COLOR;
+    private static final Color FOREGROUND_COLOR = Cell.PATH_COLOR;
+
+    private static final int RECORD_DURATION_SENTENCE_IN_SECONDS = 5;
+
     private static final String FILE_NAME = "currentGameAudio";
     private static final String AUDIO_GAME_PATH = "src/resources/audioFiles/client/audio/" + FILE_NAME + ".wav";
+
+    private static final Icon ENABLED_MIC_ICON = ImageManipulator.resizeImage(
+            "src/resources/images/game/mic_on.png", 50, 50);
+    private static final Icon DISABLED_MIC_ICON = ImageManipulator.resizeImage(
+            "src/resources/images/game/mic_off.png", 50, 50);
+    private static final Icon RECOGNIZING_ICON = ImageManipulator.resizeImage(
+            "src/resources/images/game/computing_on.png", 50, 50);
 
     private static RecognizeUserPage instance = null;
 
@@ -40,27 +67,55 @@ public class RecognizeUserPage extends JPanel implements RecordObserver {
     private RecognizeUserPage() {
         setLayout(new GridBagLayout());
         setBackground(BACKGROUND_COLOR);
+        setVisible(true);
 
+        AudioRecorder.addObserver(this);
+
+        initTriesStatus();
         initPlayerPanel();
         initRecordStatus();
+        initTimerPanel();
+        placeComp(Box.createVerticalBox(), this, 3, 1, 1, 1);
+        placeComp(Box.createHorizontalBox(), this, 1, 5, 1, 1);
 
         updateGUI();
     }
 
+    private void initTriesStatus() {
+        triesStatus = new JLabel(TRIES_STATUS_TEXT + maximumRecognitionTries);
+        triesStatus.setForeground(FOREGROUND_COLOR);
+        triesStatus.setHorizontalAlignment(JLabel.CENTER);
+        placeComp(triesStatus, this, 0, 0, 1, 1);
+    }
+
     private void initPlayerPanel() {
-        // TODO: Add colored square {Size : 2x2}
-        // TODO: Add in the center of square playerName label
+        playerPanel = new JPanel(new BorderLayout());
+        placeComp(playerPanel, this, 1, 1, 2, 2);
+
+        playerName = new JLabel();
+        playerName.setForeground(FOREGROUND_COLOR);
+        playerName.setHorizontalAlignment(JLabel.CENTER);
+        playerPanel.add(playerName, BorderLayout.CENTER);
     }
 
     private void initRecordStatus() {
-        // TODO: Same principle as in-game mic indicator
+        recordStatus = new JLabel();
+        recordStatus.setIcon(DISABLED_MIC_ICON);
+
+        placeComp(recordStatus, this, 2, 4, 1, 1);
     }
 
-    public void recognizeUser(User user) {
-        // TODO: Set playerPanel color to user color
-        // TODO: Set playerName to user name
+    private void initTimerPanel() {
+        timerPanel = new TimerPanel(RECORD_DURATION_SENTENCE_IN_SECONDS);
+        placeComp(timerPanel, this, 1, 4, 1, 1);
+    }
 
-        startRecord();
+    public void recognizePlayer(Player player, User user) {
+
+        choosenUser = user;
+
+        playerPanel.setBackground(player.getColor());
+        playerName.setText(user.getUserName());
     }
 
     private void startRecord() {
@@ -103,7 +158,23 @@ public class RecognizeUserPage extends JPanel implements RecordObserver {
     }
 
     private void isThatYou(User recognizedUser) {
-        // Open JOptionPane "Is that you?" "Yes / No"
+
+        if (recognizedUser.equals(choosenUser)) {
+            JOptionPane.showMessageDialog(this, "That's you!\nYou were successfully recognized.", "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+
+        maximumRecognitionTries--;
+
+        if (maximumRecognitionTries == 0) {
+            JOptionPane.showMessageDialog(this, "You have no more tries.\nYou were not recognized.", "Failure",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        JOptionPane.showMessageDialog(this, "That's not you!\nTry again.", "Failure",
+                JOptionPane.ERROR_MESSAGE);
     }
 
     private void updateStatus() {
@@ -119,6 +190,15 @@ public class RecognizeUserPage extends JPanel implements RecordObserver {
     public void endRecordUpdate() {
         compareAudioWithModel();
         updateStatus();
+    }
+
+    private static void placeComp(Component comp, JPanel panel, int x, int y, int w, int h) {
+        GridBagConstraints cons = new GridBagConstraints();
+        cons.gridx = x;
+        cons.gridy = y;
+        cons.gridwidth = w;
+        cons.gridheight = h;
+        panel.add(comp, cons);
     }
 
     private void updateGUI() {
